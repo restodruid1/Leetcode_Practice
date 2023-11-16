f98cd617679f9e03414f92588d56766d40bb718f
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2023-11-15 21:57:17 -0800

    Add files via upload

---
+++ LeetcodeProblems/Leetcode 75/199_Binary_Tree_Right_Side_View.py
@@ -0,0 +1,64 @@
+"""
+Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+ 
+
+Example 1:
+
+Input: root = [1,2,3,null,5,null,4]
+Output: [1,3,4]
+
+Example 2:
+
+Input: root = [1,null,3]
+Output: [1,3]
+
+Example 3:
+
+Input: root = []
+Output: []
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [0, 100].
+    -100 <= Node.val <= 100
+
+
+
+
+
+
+
+
+
+SOLUTION: """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        ans = []
+        q = collections.deque([root])
+
+        while q:
+            right_side = None
+            qlen = len(q)
+
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    right_side = node
+                    q.append(node.left)
+                    q.append(node.right)
+            if right_side:
+                ans.append(right_side.val)
+
+        return ans
+        
+#Time Complexity = O(n)
+#Space Complexity = O(n)
