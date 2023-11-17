749a1c37f792fd6159f80d51f16b101bdc0bca38
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2023-11-16 16:56:49 -0800

    Add files via upload

---
+++ LeetcodeProblems/Leetcode 75/1161_Maximum_Level_Sum_of_a_Binary_Tree.py
@@ -0,0 +1,67 @@
+"""
+Given the root of a binary tree, the level of its root is 1, the level of its children is 2, and so on.
+
+Return the smallest level x such that the sum of all the values of nodes at level x is maximal.
+
+ 
+
+Example 1:
+
+Input: root = [1,7,0,7,-8,null,null]
+Output: 2
+Explanation: 
+Level 1 sum = 1.
+Level 2 sum = 7 + 0 = 7.
+Level 3 sum = 7 + -8 = -1.
+So we return the level with the maximum sum which is level 2.
+
+Example 2:
+
+Input: root = [989,null,10250,98693,-89388,null,null,null,-32127]
+Output: 2
+
+ 
+
+Constraints:
+
+    The number of nodes in the tree is in the range [1, 104].
+    -105 <= Node.val <= 105
+
+
+
+
+
+SOLUTION: """
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxLevelSum(self, root: Optional[TreeNode]) -> int:
+        q = collections.deque([root])
+        lvl = 0
+        lvl_sum = 0
+        max_tot = float('-inf')
+        ans = 0
+        while q:
+            qlen = len(q)
+            lvl += 1
+            for i in range(qlen):
+                node = q.popleft()
+
+                if node:
+                    q.append(node.left)
+                    q.append(node.right)
+                    lvl_sum += node.val
+            
+            if lvl_sum > max_tot and q:
+                max_tot = lvl_sum
+                ans = lvl
+            lvl_sum = 0
+        
+        return ans
+
+Time Complexity = O(n) maybe O(n^2)?
+Space Complexity = O(n)
