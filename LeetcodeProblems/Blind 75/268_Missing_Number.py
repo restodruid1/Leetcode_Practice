eab96f3b2f08d0a8af330c2dfbcea4a55fbe7db2
restodruid1 <106450663+restodruid1@users.noreply.github.com>
2023-12-05 21:20:52 -0800

    Add files via upload

---
+++ LeetcodeProblems/Blind 75/268_Missing_Number.py
@@ -0,0 +1,73 @@
+"""
+Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,0,1]
+Output: 2
+Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 2:
+
+Input: nums = [0,1]
+Output: 2
+Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number in the range since it does not appear in nums.
+
+Example 3:
+
+Input: nums = [9,6,4,2,3,5,7,0,1]
+Output: 8
+Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing number in the range since it does not appear in nums.
+
+ 
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 104
+    0 <= nums[i] <= n
+    All the numbers of nums are unique.
+
+ 
+
+Follow up: Could you implement a solution using only O(1) extra space complexity and O(n) runtime complexity?
+
+
+
+
+
+"""
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        counter = Counter(nums)
+        ans = -1
+        for i in range(len(nums)):
+            if i not in counter:
+                ans = i
+        if ans == -1:
+            return len(nums)
+        else:
+            return ans
+
+#Time Complexity = O(n)
+#Space Complexity = O(n)
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        sum1 = 0
+        sum2 = 0
+        
+        for ele in nums:
+            sum1 += ele
+        
+        for ele in range(len(nums)+1):
+            sum2 += ele
+                
+
+        return sum2 - sum1
+
+#Time Complexity = O(n)
+#Space Complexity = O(1)
